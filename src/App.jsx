aadf12bfd52e32b74f60d00b4dc4a4f68585e1e5
RaylannaL <raylanna.l@hotmail.com>
2024-01-02 17:33:37 -0300

    feat: :sparkles: new component Card

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,31 @@
 /* eslint-disable import/no-anonymous-default-export */
+import "./App.css"
 import React from "react";
 
+import Card from "./components/layout/Card";
 import Aleatorio from "./components/basicos/Aleatorio";
 import Primeiro from "./components/basicos/Primeiro";
 import ComParametro from "./components/basicos/ComParametro";
 
 export default (_) => (
-  <div id="app">
+  <div className="App">
     <h1>Fundamentos React</h1>
-    <Aleatorio min = {1} max = {60}></Aleatorio>
-    <ComParametro
-      titulo="Situação do aluno:"
-      aluno="Maria"
-      nota={9.2}
-    ></ComParametro>
-    <Primeiro></Primeiro>
+    <div className="Cards">
+      <Card titulo="Exemplo de Card">
+        <Aleatorio min={1} max={60}></Aleatorio>
+      </Card>
+
+      <Card titulo="Com Parâmetro">
+        <ComParametro
+          titulo="Situação do aluno:"
+          aluno="Maria"
+          nota={9.2}
+        ></ComParametro>
+      </Card>
+
+      <Card titulo="Primeiro Componente">
+        <Primeiro></Primeiro>
+      </Card>
+    </div>
   </div>
 );
