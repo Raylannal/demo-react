65ff8d51224cd824aaf44572a4c6601c6cff37cf
RaylannaL <raylanna.l@hotmail.com>
2024-01-02 16:26:56 -0300

    feat: :sparkles: new component aleatorio

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 /* eslint-disable import/no-anonymous-default-export */
 import React from "react";
+
+import Aleatorio from "./components/basicos/Aleatorio";
 import Primeiro from "./components/basicos/Primeiro";
 import ComParametro from "./components/basicos/ComParametro";
 
 export default (_) => (
   <div id="app">
     <h1>Fundamentos React</h1>
+    <Aleatorio min = {1} max = {60}></Aleatorio>
     <ComParametro
       titulo="Situação do aluno:"
       aluno="Maria"
